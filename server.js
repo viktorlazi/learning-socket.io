d335f06fa451f9ee4b965f41ab2ed9e3b242b104
Viktorlazi <viktorlazi010@gmail.com>
2021-01-29 09:36:45 +0100

    chat is working

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ io.on('connection', (socket) => {
 
 io.on('connection', (socket) => {
   socket.on('chat message', (msg) => {
-    console.log('message: ' + msg);
+    io.emit('chat message', msg)
   });
 });
+
+
