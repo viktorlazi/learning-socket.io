0737208d08bff6cbc43652ca8fbd1aa2ead1d74d
Viktorlazi <viktorlazi010@gmail.com>
2021-01-29 09:44:52 +0100

    socket.broadcast is useful

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ io.on('connection', (socket) => {
 
 io.on('connection', (socket) => {
   socket.on('chat message', (msg) => {
-    io.emit('chat message', msg)
+    socket.broadcast.emit('chat message', msg)
   });
 });
 
